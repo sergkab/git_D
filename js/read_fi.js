041dbdaabfdc62d919a08f754bc387cd27a4b6ff
sergkab <sergkab@mail.ru>
2022-07-15 12:48:55 -0700

    node js

---
+++ js/read_fi.js
@@ -0,0 +1,11 @@
+//const fs = require('fs');
+const fs = require('fs');
+
+fs.readFile('proba.txt', 'utf-8', (err, data)=>{console.log(data);});
+//fs.readFile('proba.txt');
+
+let aa = 7;
+let bb = aa*2;
+console.log(bb);
+
+
